d6105f8e6f29124b49b2dc4cbf5a97636c49f98f
Chandler Abril <abrilchandler1@gmail.com>
2024-07-06 17:10:54 -0700

    Done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,23 @@
 # your code goes here!
+import time
+
+def countdown(integer):
+    while integer > 0:
+        print(f'{integer} SECOND(S)!')
+        integer -= 1
+        if integer == 0:
+            print("HAPPY NEW YEAR!")
+
+
+countdown(10)
+    
+def countdown_with_sleep(integer):
+    while integer > 0:
+        print(f'{integer} SECOND(S)!')
+        time.sleep(1)
+        integer -= 1
+
+    print("HAPPY NEW YEAR!")
+    
+
+countdown_with_sleep(10)
